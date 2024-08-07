92101b48b886fcc90fd875d7eec40eb9beaaffe1
byK3 <92488474+byK3@users.noreply.github.com>
2024-08-07 06:02:58 +0200

    Updated server-side performance
    Cached player data better

--- nv_scoreboard/server/server.lua
+++ nv_scoreboard/server/server.lua
@@ -8,33 +8,49 @@ AddEventHandler("nv_advanced_scoreboard:server:playerList",function()
 	TriggerClientEvent("nv_advanced_scoreboard:client:update_nui",src,nuiData)
 end)
 
+local players = {} -- new table to cache player data (xPlayer is too big!)
+
+AddEventHandler('esx:playerLoaded', function(source)
+    local xPlayer = ESX.GetPlayerFromId(source)
+    if not xPlayer then return end
+    Wait(3000)
+    local name = xPlayer.getName() or "Unknown"
+    local job = xPlayer.getJob().label or "Unknown"
+    local steamName = GetPlayerName(source) or "Unknown"
+
+    players[source] = {
+        name = name,
+        job = job,
+        steamName = steamName
+    }
+end)
+
+AddEventHandler('esx:setJob', function(source, job, lastJob)
+    players[source].job = ESX.GetPlayerFromId(source).getJob().label
+end)
+
+AddEventHandler('esx:playerDropped', function(source)
+    players[source] = nil
+end)
+
 function get_player_list()
-	while ESX == nil do
-        Citizen.Wait(500)
-    end
     local playerlist = {}
-    for _, playerId in ipairs(GetPlayers()) do
-        local ping = GetPlayerPing(playerId)
-        local job = "N/A"
-        local name = "N/A"
-
-        local xPlayer = ESX.GetPlayerFromId(playerId)
+    for k, v in pairs(players) do
+        local xPlayer = ESX.GetPlayerFromId(k)
         if xPlayer then
-            job = xPlayer.getJob().label
-            name = xPlayer.getName()
+            local ping = GetPlayerPing(k) or 0
+            table.insert(playerlist, {
+                id = k,
+                name = v.name,
+                job = v.job,
+                steamName = v.steamName,
+                ping = ping
+            })
         end
-        local steamName = GetPlayerName(playerId)
-
-        table.insert(playerlist, {
-            id = playerId,
-            name = name,
-            ping = ping,
-            steamName = steamName,
-            job = job
-        })
     end
     return playerlist
 end
+
 function string:replace(oldText, newText)
     return self:gsub(oldText, newText)
 end
