67999081dae7a01d780b888341421ae2df0b6186
šeki <seekii.official@gmail.com>
2024-08-09 11:31:53 +0200

    removed empty print in server.lua

--- nv_scoreboard/server/server.lua
+++ nv_scoreboard/server/server.lua
@@ -3,7 +3,6 @@ ESX = exports["es_extended"]:getSharedObject()
 RegisterNetEvent("nv_advanced_scoreboard:server:playerList")
 AddEventHandler("nv_advanced_scoreboard:server:playerList",function()
 	local src = source
-	print()
 	local nuiData = {type="playerlist",data=get_player_list()}
 	TriggerClientEvent("nv_advanced_scoreboard:client:update_nui",src,nuiData)
 end)
